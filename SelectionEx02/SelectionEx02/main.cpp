a8608b2e612859d1ead6231127e6bae547391e75
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-04-28 18:32:53 +0300

    Update main.cpp

--- SelectionEx02/SelectionEx02/main.cpp
+++ SelectionEx02/SelectionEx02/main.cpp
@@ -2,18 +2,6 @@
 #include <stdio.h>
 #include "SelectionProblemSolution.h"
 
-#include "Person.h"
-
-bool IsKeyIDExist(vector<Person> i_PersonArr, int i_ArrSize, int i_KeyID)
-{
-	for (int i = 0; i < i_ArrSize; i++) {
-		if (i_PersonArr[i].GetKeyID() == i_KeyID)
-			return false;
-	}
-
-	return true;
-}
-
 int main()
 {
 	SelectionSolutionProgram selectionProgram;
