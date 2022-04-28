69c22963602117aa56229a09563d07dac9b89033
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-04-28 18:25:28 +0300

    Update SelectionProblemSolution.h

--- SelectionEx02/SelectionEx02/SelectionProblemSolution.h
+++ SelectionEx02/SelectionEx02/SelectionProblemSolution.h
@@ -4,17 +4,22 @@
 #include <random>
 #include <cstdlib>
 #include <iostream>
+#include "Person.h"
+#include <vector>
+
+using namespace std;
 
 class SelectionSolutionProgram
 {
 
-
-
 public:
-	
-	const Person& SelectionRand(Person* io_PersonArr, int i_NumOfPersons, int i_RequestedPersonIndex, int& o_NumComp);
-	const Person& SelectionHeap(Person* io_PersonArr, int i_NumOfPersons, int i_RequestedPersonIndex, int& o_NumComp);
-	const Person& SelectionBST(Person* io_PersonArr, int i_NumOfPersons, int i_RequestedPersonIndex, int& o_NumComp);
-	int Partition(Person* io_PersonArr, int i_LeftIndex, int i_RightIndex);
+
+	void Run();
+	void InputProcedure(vector<Person> o_PersonArr);
+	const Person& SelectionRandWrapper(vector<Person> io_PersonArr, int i_NumOfPersons, int i_RequestedPersonIndex, int& io_NumComp);
+	const Person& SelectionRand(vector<Person> io_PersonArr, int i_LeftIndex, int i_RightIndex, int i_KPersonIndex, int& io_NumComp);
+	const Person& SelectionHeap(Person* io_PersonArr, int i_NumOfPersons, int i_KPersonIndex, int& io_NumComp);
+	const Person& SelectionBST(Person* io_PersonArr, int i_NumOfPersons, int i_KPersonIndex, int& io_NumComp);
+	int Partition(vector<Person> io_PersonArr, int i_LeftIndex, int i_RightIndex);
 
 };
