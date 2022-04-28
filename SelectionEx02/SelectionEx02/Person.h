a84995f20e94b652850baf4496d3e4386de681b0
TalAharon23 <talaha@mta.ac.il>
2022-04-28 15:56:12 +0300

    Starting Person class and Heap class

--- SelectionEx02/SelectionEx02/Person.h
+++ SelectionEx02/SelectionEx02/Person.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <random>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+	int m_KeyID;
+	string m_Name;
+
+public:
+	int GetKeyID() { return m_KeyID; }
+	string GetName() { return m_Name; }
+};
