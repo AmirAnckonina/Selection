4bca3e05314c89a96cb7bf5a6bf0de9d5f311e93
TalAharon23 <talaha@mta.ac.il>
2022-05-07 11:48:12 +0300

    Fixing the selection problem with heap

---
+++ SelectionEx02/enc_temp_folder/d8b81976ba715e693ed77c12d691609/main.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "Person.h"
+
+bool IsKeyIDExist(vector<Person> i_PersonArr, int i_ArrSize, int i_KeyID)
+{
+	for (int i = 0; i < i_ArrSize; i++) {
+		if (i_PersonArr[i].GetKeyID() == i_KeyID)
+			return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	int n, keyID;
+	string name;
+	vector<Person> personArr;
+
+	cout << "Please enter the number of person: ";
+	cin >> n;
+
+	cout << "Please enter " << n << " pepole:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Enter " << "#" << i << " person: [ID, Name]" << endl;
+		cin >> keyID >> name;
+		while (IsKeyIDExist(personArr, n, keyID))
+		{
+			cout << "Invalid input, ID already exist. Try again:" << endl;
+			cout << "Enter person #" << i << ": ";
+			cin >> keyID >> name;
+		}
+		personArr.push_back({ keyID, name });
+	}
+
+
+	return 0;
+}
