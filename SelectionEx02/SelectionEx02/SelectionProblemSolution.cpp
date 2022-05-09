7d5414afacf10b3cda24f99c95bd977d35172baa
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-05-09 18:35:50 +0300

    Update SelectionProblemSolution.cpp

--- SelectionEx02/SelectionEx02/SelectionProblemSolution.cpp
+++ SelectionEx02/SelectionEx02/SelectionProblemSolution.cpp
@@ -2,118 +2,70 @@
 
 void SelectionSolutionProgram::Run()
 {
-	InputProcedure();
-	m_KthPerson = GetKthPerson();
-	Person selectedPerson = SelectionRandWrapper(m_PersonArr, m_NumOfPersons, m_KthPerson);
-	selectedPerson.PrintPersonDetails();
-}
-
-int SelectionSolutionProgram::GetKthPerson()
-{
+	vector<Person*> personArr;
 	int kthPerson;
-
-	cout << "Please enter the 'k' largest Person you want by key :";
-	cin >> kthPerson;
-
-	return kthPerson;
+	int numOfPersons;
+	int numComp = 0;
+	InputProcedure(personArr, numOfPersons);
+	kthPerson = GetKthPerson();
+	const Person* selectedPerson = SelectionRandWrapper(personArr, numOfPersons, kthPerson, numComp);
 }
 
-void SelectionSolutionProgram::InputProcedure()
+const Person* SelectionSolutionProgram::SelectionRandWrapper(vector<Person*>& io_PersonArr, int i_NumOfPersons, int i_KPerson, int& io_NumComp)
 {
-
-	int keyID;
-	istringstream iss;
-	string fullName;
-	string firstName;
-	string lastName;
-	string line;
-
-
-	cout << "Please enter the number of person: ";
-	cin >> m_NumOfPersons;
-	cin.ignore();
-
-	cout << "Please enter " << m_NumOfPersons << " pepole:" << endl;
-	for (int i = 0; i < m_NumOfPersons; i++)
-	{
-		cout << "Enter " << "#" << i + 1 << " person: [ID, Name]" << endl;
-		getline(cin, line);
-		iss.clear();
-		iss.str(line);
-		iss >> keyID;
-		iss >> firstName;
-		iss >> lastName;
-		fullName = firstName + " " + lastName;
-		if (IsKeyIDExist(m_PersonArr, keyID))
-		{
-			cout << "Invalid input, ID already exist." << endl;
-			exit(1);
-		}
-		m_PersonArr.push_back({ keyID, fullName });
-	}
+	/// We're sending the K person minus 1 so we will reach the relevant index.
+	io_NumComp = 0;
+	return SelectionRand(io_PersonArr, 0, i_NumOfPersons - 1, i_KPerson - 1, io_NumComp);
 }
 
-bool SelectionSolutionProgram::IsKeyIDExist(vector<Person> i_PersonArr, int i_KeyID)
-{
-	for (int i = 0; i < i_PersonArr.size(); i++) {
-		if (i_PersonArr[i].GetKeyID() == i_KeyID)
-			return true;
-	}
-
-	return false;
-}
-
-const Person& SelectionSolutionProgram::SelectionRandWrapper(vector<Person> io_PersonArr, int i_NumOfPersons, int i_KPersonIndex)
-{
-	m_NumComp = 0;
-	return SelectionRand(io_PersonArr, 0, i_NumOfPersons - 1, i_KPersonIndex, m_NumComp);
-}
-
-const Person& SelectionSolutionProgram::SelectionRand(vector<Person> io_PersonArr, int i_LeftIndex, int i_RightIndex, int i_KPersonIndex, int& io_NumComp)
+const Person* SelectionSolutionProgram::SelectionRand(vector<Person*>& io_PersonArr, int i_LeftIndex, int i_RightIndex, int i_KPersonIndex, int& io_NumComp)
 {
 	int pivotIndex;
 
 	pivotIndex = Partition(io_PersonArr, i_LeftIndex, i_RightIndex);
-	if (i_KPersonIndex == pivotIndex + 1)
+	if (i_KPersonIndex == pivotIndex)
 	{
 		return io_PersonArr[pivotIndex];
 	}
 
-	else if (i_KPersonIndex < pivotIndex + 1)
+	else if (i_KPersonIndex < pivotIndex)
 	{
 		i_RightIndex = pivotIndex - 1;
 		return SelectionRand(io_PersonArr, i_LeftIndex, i_RightIndex, i_KPersonIndex, io_NumComp);
 	}
 
-	else /// i_KPersonIndex > pivotIndex + 1
+	else /// i_KPersonIndex > pivotIndex
 	{
 		i_LeftIndex = pivotIndex + 1;
-		/// i_KPersonIndex -= pivotIndex;
 		return SelectionRand(io_PersonArr, i_LeftIndex, i_RightIndex, i_KPersonIndex, io_NumComp);
 	}
 }
 
-int SelectionSolutionProgram::Partition(vector<Person> io_PersonArr, int i_LeftIndex, int i_RightIndex)
+int SelectionSolutionProgram::Partition(vector<Person*>& io_PersonArr, int i_LeftIndex, int i_RightIndex)
 {
-	int randomPivot = rand() % (i_RightIndex - i_LeftIndex + 1); /// Check
+	int randomPivot; /// = rand() % (i_RightIndex - i_LeftIndex + 1); /// Check
+	randomPivot = i_LeftIndex + (rand() % (i_RightIndex - i_LeftIndex));
+	int choosedPivotCurrentIndex;
 	bool pivotIsLeftToNonPivot;
-	/// Person personInPivot = io_PersonArr[randomPivot]; 
 
 	swap(io_PersonArr[randomPivot], io_PersonArr[i_LeftIndex]); /// So now the most left is the pivot and we can start the partition.
 	pivotIsLeftToNonPivot = true;
+	choosedPivotCurrentIndex = i_LeftIndex;
 	while (i_LeftIndex < i_RightIndex)
 	{
-		if (io_PersonArr[i_LeftIndex].GetKeyID() > io_PersonArr[i_RightIndex].GetKeyID())
+		if (io_PersonArr[i_LeftIndex]->GetKeyID() > io_PersonArr[i_RightIndex]->GetKeyID())
 		{
 			swap(io_PersonArr[i_LeftIndex], io_PersonArr[i_RightIndex]);
 			if (pivotIsLeftToNonPivot)
 			{
+				choosedPivotCurrentIndex = i_RightIndex;
 				i_LeftIndex++;
 				pivotIsLeftToNonPivot = false;
 			}
 
 			else
 			{
+				choosedPivotCurrentIndex = i_LeftIndex;
 				i_RightIndex--;
 				pivotIsLeftToNonPivot = true;
 			}
@@ -133,7 +85,110 @@ int SelectionSolutionProgram::Partition(vector<Person> io_PersonArr, int i_LeftI
 		}
 	}
 
-	return i_LeftIndex;
+	return choosedPivotCurrentIndex;
 }
 
+void SelectionSolutionProgram::InputProcedure(vector<Person*>& o_PersonArr, int& o_ArrSize)
+{
+	Person* newPerson;
+
+	o_ArrSize = GetNumOfPersonsProcedure();
+
+	cout << "Please enter " << o_ArrSize << " pepole:" << endl;
+	for (int i = 0; i < o_ArrSize; i++)
+	{
+		newPerson = SinglePersonInputProcedure(o_PersonArr);
+		o_PersonArr.push_back(newPerson);
+	}
+}
+
+int SelectionSolutionProgram::GetNumOfPersonsProcedure()
+{
+	int numOfPersons;
+
+	cout << "Please enter the number of persons: ";
+	cin >> numOfPersons;
+	if (numOfPersons == NULL)
+	{
+		throw new exception("Invalid num of persons input");
+	}
+
+	cin.ignore();
+
+	return numOfPersons;
+}
+
+void SelectionSolutionProgram::NameValidation(string i_Name)
+{
+	if (i_Name.length() == 0) 
+	{
+		throw new exception("Invalid name!");
+	}
+
+	/// Add validation to the chars (contain only letters).
+}
+
+void SelectionSolutionProgram::KeyIDValidation(vector<Person*> i_PersonArr, int i_KeyID)
+{
+	/// Check Line content.
+	if (i_KeyID == 0) 
+	{
+		throw new exception("Invalid key ID!");
+	}
+
+	if (IsKeyIDExist(i_PersonArr, i_KeyID))
+	{
+		throw new exception("Invalid input, ID already exist.");
+	}
+}
+
+bool SelectionSolutionProgram::IsKeyIDExist(vector<Person*> i_PersonArr, int i_KeyID)
+{
+	for (int i = 0; i < i_PersonArr.size(); i++) {
+		if (i_PersonArr[i]->GetKeyID() == i_KeyID)
+			return true;
+	}
+
+	return false;
+}
+
+Person* SelectionSolutionProgram::SinglePersonInputProcedure(vector <Person*> i_PersonArr)
+{
+	int keyID;
+	istringstream iss;
+	string tempNamePart, fullName, line;
+	Person* newPerson;
+
+	cout << "Enter person ID and Name" << endl;
+	getline(cin, line);
+	iss.clear();
+	iss.str(line);
+	fullName.clear();
+	iss >> keyID;
+	KeyIDValidation(i_PersonArr, keyID);
+
+	iss >> tempNamePart;
+	fullName += tempNamePart;
+	while (!iss.eof()) 
+	{
+		fullName += ' ';
+		iss >> tempNamePart;
+		fullName += tempNamePart;
+	}
+
+	NameValidation(fullName);
+	newPerson = new Person(keyID, fullName);
+
+	return newPerson;
+}
+
+int SelectionSolutionProgram::GetKthPerson()
+{
+	int reskthPerson;
+
+	cout << "Please enter the 'k' largest Person you want by key :";
+	cin >> reskthPerson;
+
+	return reskthPerson;
+}
 
