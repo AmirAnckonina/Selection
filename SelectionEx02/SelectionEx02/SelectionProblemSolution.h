691d7868c550cc6f44ea17d95046714b6e1367e1
AmirAnckonina <50033620+AmirAnckonina@users.noreply.github.com>
2022-05-09 18:35:16 +0300

    Update SelectionProblemSolution.h

--- SelectionEx02/SelectionEx02/SelectionProblemSolution.h
+++ SelectionEx02/SelectionEx02/SelectionProblemSolution.h
@@ -5,9 +5,9 @@
 #include <cstdlib>
 #include <iostream>
 #include <sstream>
-#include "Person.h"
 #include <vector>
 #include <string>
+#include "Person.h"
 
 using namespace std;
 
@@ -15,20 +15,24 @@ class SelectionSolutionProgram
 {
 
 private:
-	vector<Person> m_PersonArr;
+	/*vector<Person> m_PersonArr;
 	int m_KthPerson;
 	int m_NumOfPersons;
-	int m_NumComp;
+	int m_NumComp;*/
 
 public:
 	void Run();
-	void InputProcedure();
+	void InputProcedure(vector<Person*>& o_PersonArr, int& o_numOfPersons);
 	int GetKthPerson();
-	bool IsKeyIDExist(vector<Person> i_PersonArr, int i_KeyID);
-	const Person& SelectionRandWrapper(vector<Person> io_PersonArr, int i_NumOfPersons, int i_RequestedPersonIndex);
-	const Person& SelectionRand(vector<Person> io_PersonArr, int i_LeftIndex, int i_RightIndex, int i_KPersonIndex, int& io_NumComp);
-	const Person& SelectionHeap(Person* io_PersonArr, int i_NumOfPersons, int i_KPersonIndex, int& io_NumComp);
-	const Person& SelectionBST(Person* io_PersonArr, int i_NumOfPersons, int i_KPersonIndex, int& io_NumComp);
-	int Partition(vector<Person> io_PersonArr, int i_LeftIndex, int i_RightIndex);
+	bool IsKeyIDExist(vector<Person*> i_PersonArr, int i_KeyID);
+	const Person* SelectionRandWrapper(vector<Person*>& io_PersonArr, int i_NumOfPersons, int i_KPerson, int& io_NumComp);
+	const Person* SelectionRand(vector<Person*>& io_PersonArr, int i_LeftIndex, int i_RightIndex, int i_KPersonIndex, int& io_NumComp);
+	/*const Person& SelectionHeap(vector<Person*> io_PersonArr, int i_NumOfPersons, int i_KPersonIndex, int& io_NumComp);
+	const Person& SelectionBST(vector<Person*> io_PersonArr, int i_NumOfPersons, int i_KPersonIndex, int& io_NumComp);*/
+	int Partition(vector<Person*>& io_PersonArr, int i_LeftIndex, int i_RightIndex);
+	int GetNumOfPersonsProcedure();
+	Person* SinglePersonInputProcedure(vector <Person*> i_PersonArr);
+	void KeyIDValidation(vector<Person*> i_PersonArr, int i_KeyID);
+	void NameValidation(string i_Name);
 
 };
